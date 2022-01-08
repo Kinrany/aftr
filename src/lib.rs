1edec7b31115d568fd5a25d8e1a77e2b57609cc1
Ruslan Fadeev <kinrany@yandex.com>
2022-01-09 02:20:04 +0300

    parse newlines and tabs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use nom::{
     branch::alt,
-    character::complete::{alphanumeric1, char},
+    character::complete::{alphanumeric1, char, newline},
     combinator::{all_consuming, eof, map, value},
     multi::many1,
     sequence::terminated,
@@ -31,8 +31,16 @@ fn token_slash(input: &str) -> NResult<Token> {
     value(Token::Slash, char('/'))(input)
 }
 
+fn token_newline(input: &str) -> NResult<Token> {
+    value(Token::Newline, newline)(input)
+}
+
+fn token_tab(input: &str) -> NResult<Token> {
+    value(Token::Tab, char('\t'))(input)
+}
+
 fn token(input: &str) -> NResult<Token> {
-    alt((token_word, token_slash))(input)
+    alt((token_word, token_slash, token_newline, token_tab))(input)
 }
 
 pub fn lexer(input: &str) -> NResult<Vec<Token>> {
@@ -46,19 +54,49 @@ mod tests {
     #[test]
     fn single_word() {
         assert_eq!(
-            lexer("singleword"),
-            Ok(("", vec![Token::word("singleword")]))
+            lexer("singleword").unwrap().1,
+            vec![Token::word("singleword")]
         );
     }
 
     #[test]
     fn two_words_and_slash() {
         assert_eq!(
-            lexer("two/words"),
-            Ok((
-                "",
-                vec![Token::word("two"), Token::Slash, Token::word("words")]
-            ))
+            lexer("two/words").unwrap().1,
+            vec![Token::word("two"), Token::Slash, Token::word("words")]
+        )
+    }
+
+    #[test]
+    fn three_words_and_newline() {
+        assert_eq!(
+            lexer("three/words\nw/newline").unwrap().1,
+            vec![
+                Token::word("three"),
+                Token::Slash,
+                Token::word("words"),
+                Token::Newline,
+                Token::word("w"),
+                Token::Slash,
+                Token::word("newline")
+            ]
+        )
+    }
+
+    #[test]
+    fn four_words_with_tab() {
+        assert_eq!(
+            lexer("four/words\n\twith/tab").unwrap().1,
+            vec![
+                Token::word("four"),
+                Token::Slash,
+                Token::word("words"),
+                Token::Newline,
+                Token::Tab,
+                Token::word("with"),
+                Token::Slash,
+                Token::word("tab")
+            ]
         )
     }
 }
